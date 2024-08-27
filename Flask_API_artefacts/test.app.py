5650af98b9c5bdb4fbab02e629acad5ff4093d37
Mschap <65339152+Mschapxb@users.noreply.github.com>
2024-08-27 14:57:11 +0200

    Add tests for Flask API and update GitHub Actions workflow

---
+++ Flask_API_artefacts/test.app.py
@@ -0,0 +1,30 @@
+import json
+from app import app
+
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Boston House Price Prediction" in response.data
+
+def test_predict_api():
+    tester = app.test_client()
+    response = tester.post('/predict_api', json={
+        "data": {
+            "CRIM": 0.00632,
+            "ZN": 18.0,
+            "INDUS": 2.31,
+            "NOX": 0.538,
+            "RM": 6.575,
+            "Age": 65.2,
+            "DIS": 4.09,
+            "RAD": 1.0,
+            "TAX": 296.0,
+            "PTRATIO": 15.3,
+            "B": 396.9,
+            "LSTAT": 4.98
+        }
+    })
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert type(data) == float
